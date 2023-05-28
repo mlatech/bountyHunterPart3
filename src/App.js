ea72a63d5d2d8e796192376f0a1fa06e113aa277
Karyna Chernyak <mlatech@icloud.com>
2023-05-27 21:23:19 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Bounty from "./components/Bounty"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App(){
+  const [bounties, setBounties] = useState([])
+
+  function getBounties(){
+    axios.get("/bounties")
+    .then(res => setBounties(res.data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getBounties()
+    }, [])
+  return(
+    <div className="bounty-container">
+      {bounties.map(bounty => <Bounty {...bounty} key={bounty.firstname}/>)}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/AddBountyForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddBountyForm(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
