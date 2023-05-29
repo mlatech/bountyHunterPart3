ecec4f1310b6cfa2636748bb4e9bcbc30e6aa60a
Karyna Chernyak <mlatech@icloud.com>
2023-05-29 11:32:08 -0500

    commit

--- src/components/Bounty.js
+++ src/components/Bounty.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState}from "react";
 
 export default function Bounty(props){
-    console.log(props)
+    const {firstname, lastname, living, bounty, type, _id} = props
+    const [editToggle, setEditToggle] = useState(false)
+
 return(
-    <div>
-        
+    <div className="bounty">
+      <h2>{firstname} {lastname}</h2>
     </div>
 )
 }
